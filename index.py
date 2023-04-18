bc412e18dd8f4a07f6266e9e543e0b74c2e5bc9c
Bannikov Andrey <andrey27x777@gmail.com>
2023-04-18 21:34:19 +0300

    Added recursive

--- index.py
+++ index.py
@@ -1,13 +1,3 @@
-# import os
-# from PIL import ImageTk, Image
-# import tkinter as tk
-# from tkinter import ttk
-
-# root = tk.Tk()
-# root.title("METANIT.COM")
-# root.geometry("250x200")
-
-
 # # выбранная тема
 # selected_theme = tk.StringVar()
 # style = ttk.Style()
@@ -24,62 +14,67 @@
 #         text=theme, value=theme, variable=selected_theme, command=change_theme
 #     ).pack(anchor=tk.NW)
 
-
-# def dismiss(window):
-#     window.grab_release()
-#     window.destroy()
-
-
-# def click():
-#     window = tk.Toplevel()
-#     window.title("Новое окно")
-#     window.attributes("-fullscreen", True)
-#     # window.attributes("-toolwindow", True)
-#     window.protocol(
-#         "WM_DELETE_WINDOW", lambda: dismiss(window)
-#     )  # перехватываем нажатие на крестик
-
-#     close_button = ttk.Button(
-#         window, text="Закрыть окно", command=lambda: dismiss(window)
-#     )
-#     # close_button.pack(anchor="center", expand=1)
-#     close_button.pack()
-
-#     # cur_dir = os.getcwd()
-#     cur_dir = ""
-#     # img = PhotoImage(file=cur_dir)
-
-#     # label = ttk.Label(window, image=img)
-#     # label.pack()
-#     # print(label.winfo_parent())
-
-#     canv = tk.Canvas(window, width=2000, height=2000, bg="white")
-
-#     # canv.grid(row=2, column=3)
-#     canv.pack()
-#     img = ImageTk.PhotoImage(Image.open(cur_dir))  # PIL solution
-#     canv.create_image(0, 0, anchor=tk.NW, image=img)
-
-#     window.grab_set()  # захватываем пользовательский ввод
-
-
-# open_button = ttk.Button(text="Создать окно", command=click)
-# open_button.pack(anchor="center", expand=1)
-
-# root.mainloop()
-
+import os
 import tkinter as tk
+from tkinter import ttk
 from PIL import ImageTk, Image
+
+
+def dismiss(window):
+    window.grab_release()
+    window.destroy()
+
+
+def click():
+    def clock():
+        global index
+
+        index += 1
+
+        if index == count:
+            index = 0
+
+        label.config(image=images[index])
+        label.after(1000, clock)
+
+    window = tk.Toplevel()
+    window.title("Новое окно")
+    window.attributes("-fullscreen", True)
+    # window.attributes("-toolwindow", True)
+
+    window.protocol(
+        "WM_DELETE_WINDOW", lambda: dismiss(window)
+    )  # перехватываем нажатие на крестик
+
+    close_button = ttk.Button(
+        window, text="Закрыть окно", command=lambda: dismiss(window)
+    )
+    # close_button.pack(anchor="center", expand=1)
+    close_button.pack()
+
+    label = ttk.Label(window, image=images[0])
+    label.pack()
+
+    window.grab_set()  # захватываем пользовательский ввод
+
+    clock()
+
 
 root = tk.Tk()
 
-canv = tk.Canvas(root, width=2000, height=2000, bg="white")
-# canv.grid(row=2, column=3)
-canv.pack()
-
 cur_dir = ""
+items = os.listdir(cur_dir)
+count = len(items)
+index = 0
 
-img = ImageTk.PhotoImage(Image.open(cur_dir))  # PIL solution
-canv.create_image(0, 0, anchor=tk.NW, image=img)
+images = []
+
+for item in items:
+    images.append(ImageTk.PhotoImage(Image.open(cur_dir + "/" + item)))
+
+label = ""
+
+open_button = ttk.Button(text="Создать окно", command=click)
+open_button.pack(anchor="center", expand=1)
 
 root.mainloop()
